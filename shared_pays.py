02d46ee36732c30351a34b530b6fbd8dbf5a7df6
jmt4384 <mistera5@seznam.cz>
2024-11-24 18:24:42 +0100

    Add files via upload

--- shared_pays.py
+++ shared_pays.py
@@ -3,41 +3,97 @@ import csv
 def select_main_function():
     '''
     základní rozhodování programu - výběr funkce
-    výstupem bude menu - tj. číslo, kter definuje jaká funkce programu bude volána
+    
+    výstupem bude menu - tj. číslo, kter definuje jaká ze základních funkcí
+    programu bude volána
     '''
     while True:
-        menu=input("Pro výběr funkce zvol číslo: 1234\n")
+        #zobrazení uživateli hlavního menu a jeho výběr požadované funkce
+        print("v hlavní nabídce jsou dostupné tyto možnosti")
+        for key, value in nabidka_menu.items():  
+            print(f"možnost: {key} funkce: {value}")
+        menu=input("Pro výběr funkce zvol číslo: 01234\n")
         try:
             int(menu)
             if int(menu) in nabidka_menu.keys():
                 menu=int(menu)
+                print(f"zvolil si {menu}, funkci {nabidka_menu[menu]}")
                 break
         except:
-            print("zvolil si nepovolený znak, povelené jsou pouze čísla 1234")
+            print("zvolil si nepovolený znak, povelené jsou pouze čísla 01234")
     return menu
 
 def select_edit_function():
+    '''
+    sekundární rozhodování programu - výběr editojící funkce funkce
+    
+    výstupem bude menu - tj. číslo, kter definuje jaká z editujících funkcí
+    '''
     for key, value in editace_zaznamu.items():
-        print(f"zvol možnost {key} pro provedení funkce {value}")
+        print(f"možnost: {key} funkce: {value}")
     while True:
         menu=input("Pro výběr funkce zvol číslo: 12345\n")
         try:
             int(menu)
             if int(menu) in nabidka_menu.keys():
                 menu=int(menu)
+                print(f"zvolil si {menu}, funkci {nabidka_menu[menu]}")
                 break
         except:
             print("zvolil si nepovolený znak, povelené jsou pouze čísla 1234")
     return menu
 
+def main_function_decision():
+    '''
+    hlavní rozhodovací smyčka, po provedení funkce, program čeká na povel
+    k provedení nové funkce popř. exit
+    '''
+    while True:
+        menu=select_main_function()
+        if menu==1:                 #načtení vstupního souboru
+            data=import_input_file()
+            print("data byla importována, nyní může být vytvořen balance sheet")
+            import_done=True
+        if menu==2 and import_done:        #vytvoření balance sheetu podle lidí
+            create_balance_sheet_payer(data)
+        if menu==3 and import_done:         #vytvoření balance sheetu podle času
+            create_balance_sheet_time(data)
+        if menu==4 and import_done:         #editace záznamů
+            edit_function_decision()
+        if menu==5 and import_done:         #uložení balance sheetu
+            save_balance_sheet()
+        if menu==0:
+            break
+        print("----------------------")
+        
+def edit_function_decision():
+    '''
+    vedlejší rozhodovací smyčka, po provedení editovací funkce, 
+    program čeká na povel k provedení další funkce popř. exit do hlavního menu
+    '''
+    while True:
+        menu=select_edit_function()
+        if menu==0:
+            break
+        if menu==1:
+            add_new_member(data)
+        if menu==2:
+            remove_member(data)
+        if menu==3:
+            add_pay(data)
+        if menu==4:
+            edit_pay(data)
+        if menu==5:
+            remove_pay(data)
+        print("----------------------")
         
 def import_input_file():
     '''
-    načtení vtupního souboru databáze
-    vstupní parametr cesta k souboru
-        funkce musí provést ověření, setřídění, normalizaci
-    výstupem budou data v paměti "živý obraz databáze"
-    vstupní soubor bude přepsán normalizovanými daty
+    načtení vtupního souboru databáze, předpokládám, že existuje soubor
+    input_data.csv - jinak vyhodí hlášku "soubor nenalezen"
+    funkce provede ověření, setřídění, normalizaci dat
+    
+    výstupem bude dictionary "data" 
     '''
     data=[]
     try:
@@ -47,33 +103,31 @@ def import_input_file():
             count=1
             for radek in reader:
                 count+=1
-                normalised_row=normalise_pay_data(radek)
                 #normalizace, osekání řádků a úprava textů, def měna = CZK
-                
+                normalised_row=normalise_pay_data(radek)              
                 #validace záznamu - musí být vždy alespoň kdo platil a kolik
                 #jinak je řádek přeskočen
                 if not validate_row(normalised_row):
                     print(f"chybný záznam č. {count} přeskočen")
                     continue
-                if normalised_row["currency"]=="CZK":
-                    norm_amount=normalised_row["currency"]
-                if normalised_row["currency"]=="EUR":
-                    norm_amount=exchange_eur(normalised_row["currency"])
-                if normalised_row["currency"]=="TL":
-                    norm_amount=exchange_tl(normalised_row["currency"])                        
-                radek.append(norm_amount)
                 data.append(radek)
         print(data)
-        print("vstupní soubor je řádně upraven, připraveno k přepsání vstupu")
-        #rewrite_input_file()
+        print("vstupní soubor je řádně upraven")
     except:
         print("vstupní soubor nenalezen")
         return None
+    return data
 
 def normalise_pay_data(pay_data):
-    '''osekání polí, která nepotřebuji'''
+    '''konverze řádky ve vstupním souboru tak, aby obsahoval pouze 
+    požadovaná data
+    
+    výstup je normalizovaná řádka
+    '''
+    
     pay_data_str={key:str(value) for key,value in pay_data.items() if key in CSV_FIELD_NAME}
     ''''doplnění polí do plné délky'''
+    
     normalised_data={
         "date_time":pay_data_str["date_time"].strip() if pay_data_str.get("date_time") else "nevyplněno",
         "payer":pay_data_str["payer"].strip().capitalize() if pay_data_str.get("payer") else "nevyplněno",
@@ -82,28 +136,66 @@ def normalise_pay_data(pay_data):
         "currency":pay_data_str["currency"].strip() if pay_data_str.get("currency") else "CZK",
         }
     return normalised_data
+
 def validate_row(row):
+    '''
+    ověření, že má řádka validní parametry kdo platil, částka
+    výstup T/F - platný neplatný řádek v datech
+    '''
     if row["payer"]=="" or row["amount"]=="":
         return False
     return True
+
 def exchange_eur(eur):
+    '''
+    převod měny EUR na CZK
+    '''
     return EXCHANGE_E_CZK*eur
+
 def exchange_tl(tl):
-    return EXCHANGE_TL_CZK*tl    
+    '''
+    převod měny TL na CZK
+    '''
+    return EXCHANGE_TL_CZK*tl  
+  
+def norm_amount(radek):
+    
+    if radek["currency"]=="CZK":
+        norm_amount=radek["currency"]
+    elif radek["currency"]=="EUR":
+        norm_amount=exchange_eur(radek["currency"])
+    elif radek["currency"]=="TL":
+        norm_amount=exchange_tl(radek["currency"])
+    else:                 #pokud je na znaku měny něco jiného, pak jsou to CZK
+        norm_amount=radek["currency"]
+   
+    return norm_amount
+
+        
 def rewrite_input_file():
     '''
     funkce na přepsání vstupního souboru vyčištěnými normalizovanými daty
     '''
-def create_balance_sheet():
+def create_balance_sheet_payer(data):
     '''
-    vytvoření balance sheetu
-    možnosti zobrazení: setříděné podle času zadání/ podle osob
+    vytvoření balance sheetu setříděné podle podle osob
     zobrazení na obrazovku
     možnost: 
         výstup do souboru --- volání funkce 3
         volba pokud nesouhlasíš, volej funkci "editace záznamů"
     '''
+    #přidání sloupečku jednotné měny
+    for radek in data:
+        radek["def_currency"]=norm_amount(radek)
+    
     print("tady bude vytvořen balance sheet")
+    print(data)
+    
+    
+def create_balance_sheet_time():
+    '''
+    balancesheet setříděný podle času
+    '''
 def add_new_member():
     '''
     funkce přidá nového člena do živé databáze plus jeho platby
@@ -114,6 +206,7 @@ def remove_member():
     funkce odebere člena a všechny jeho platby
     možnost --- aktualizuj balance sheet
     '''
+    print("tady bude funkce pro odebrání člena party")
 def add_pay():
     '''
     funkce přidá záznam do živých načtených dat
@@ -146,12 +239,14 @@ hlavní menu projektu
 '''možnost doplnit - vytvoření vstupního souboru, '''
 
 nabidka_menu={
+    0:"exit",
     1:"načtení vtupního souboru databáze",
     2:"vytvoření balance sheetu",
     3:"editace záznamů",
     4:"uložení balance sheetu a přepsání vstupního souboru normalizovanými daty",
     }
 editace_zaznamu={
+    0:"exit do hlavního menu",
     1:"přidání nového člena",
     2:"odebrání stávajícího člena",
     3:"přidání platby",
@@ -165,40 +260,11 @@ print('''Vítejte v programu "shared Pays"\n''')
 print("Tento program slouží k přepočtu sdílených nákladů" 
 "na společný projekt/ výlet více účastníků a rovnému vyrovnání mezi nimi\n")
 
-#zobrazení uživateli hlavního menu a jeho výběr požadované funkce
-print("v hlavní nabídce jsou dostupné tyto možnosti")
+main_function_decision()
 
-for key, value in nabidka_menu.items():  
-    print(f"zvol možnost {key} pro provedení funkce {value}")
 
-#print(nabidka_menu.keys())
-#print(1 in nabidka_menu.keys())
-menu=select_main_function()
-print(f"zvolil sis funkci {menu}, tj funkci {nabidka_menu[menu]}")
-
-if menu==1:                 #načtení vstupního souboru
-    import_input_file()
-    print("data byla importována, nyní bude vytvořen balance sheet")
-    menu=2
-
-if menu==2:                 #vytvoření balance sheetu
-    create_balance_sheet()
-
-if menu==3:                 #editace záznamů
-    menu=select_edit_function()
-    if menu==1:
-        add_new_member()
-    if menu==2:
-        remove_member()
-    if menu==3:
-        add_pay()
-    if menu==4:
-        edit_pay()
-    if menu==5:
-        remove_pay()
-
-if menu==4:                 #uložení balance sheetu
-    save_balance_sheet()
+   
+print("program byl řádně ukončen užiatelem")
 
 
 
